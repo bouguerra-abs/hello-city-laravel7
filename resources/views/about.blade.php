1cb6987c1c62bf5404f99fc27301db57eac7b6ef
bouguerra-abs <bouguerraabdsatar2050@gmail.com>
2020-12-15 22:33:03 +0100

    add about page and edit links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,13 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>About Us | Hello City</title>
 
     </head>
     <body>
-        <h1>Hello from tunisia</h1>
+        <p>Built with &hearts; by STOURIDGE.</p>
 
-        <p>It's currently {{ date('h:i A') }}.</p>
+        <p><a href="/">Revenir à la page d'accueil</a></p>
 
         <footer>
             <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About Us</a></p>

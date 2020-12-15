d89b908be7619ac9fa7144a36a3b0688ddc2df6c
bouguerra-abs <bouguerraabdsatar2050@gmail.com>
2020-12-15 22:52:59 +0100

    Remove duplication by using layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About Us | Hello City</title>
-
-    </head>
-    <body>
+@section('content')
         <p>Built with &hearts; by STOURIDGE.</p>
 
         <p><a href="/">Revenir à la page d'accueil</a></p>
 
-        <footer>
-            <p>&copy; Copyright {{ date('Y') }}</p>
-        </footer>
-    </body>
-</html>
+@endsection
+    

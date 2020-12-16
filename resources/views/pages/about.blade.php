50a26415376db97b256a4523390f9c9d82d593d2
bouguerra-abs <bouguerraabdsatar2050@gmail.com>
2020-12-16 22:01:52 +0100

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,6 +5,8 @@
 
 @section('content')
 
+<img src="/images/Rasmus-lerdorf.jpg" alt="STOURIDGE Logo" style="width:200px;height:200px;border-radius: 50%">
+
     <p>Built with &hearts; by STOURIDGE.</p>
 
     <p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,6 +4,8 @@
 
 
 @section('content')
+	<img src="/images/map.jpg" alt="Tunis Flag" style="width:200px;height:200px;border-radius: 50%">
+
     <h1>Hello from tunisia</h1>
 
     <p>It's currently {{ date('h:i A') }}.</p>
